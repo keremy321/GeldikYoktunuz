e9f2966ab15f5756c901d854b52a4350c969a603
integrateThem <ahmedfaruker19@gmail.com>
2024-12-24 01:46:30 +0300

    Rota görselleştirmesi

--- src/main/java/org/geldikYoktunuz/ImprovedGraphStreamExample.java
+++ src/main/java/org/geldikYoktunuz/ImprovedGraphStreamExample.java
@@ -15,14 +15,45 @@ public class ImprovedGraphStreamExample {
         CityGraph cityGraph = new CityGraph();
         setupCities(cityGraph); // Populate CityGraph with cities and routes
 
+
+        Customer customer1 = new Customer("Ahmed Er", "Faruk", "/dialogButtons/man.png");
+        Customer customer2 = new Customer("Emre", "KarateKid", "/dialogButtons/woman.png");
+        Customer customer3 = new Customer("Mahmut", "Mahmutoğlu", "/dialogButtons/man.png");
+        Customer customer4 = new Customer("İbrahim", "Çetin", "/dialogButtons/woman.png");
+        Cargo cargo1 = new Cargo( CurrentDate.currentDate.plusDays(1), false, "Mehmet Aydın", "Photo","Cargo1", CityStorage.getCityById(35));
+        Cargo cargo2 = new Cargo( CurrentDate.currentDate.plusDays(1), false, "Mehmet Aydın", "Photo","Cargo2",CityStorage.getCityById(10));
+        Cargo cargo3 = new Cargo( CurrentDate.currentDate.plusDays(1), false, "Ahmet Yılmaz", "Photo","Cargo3",CityStorage.getCityById(41));
+        Cargo cargo4 = new Cargo( CurrentDate.currentDate, false, "Semih Bekdaş", "Photo","Cargo4",CityStorage.getCityById(45));
+        Cargo cargo5 = new Cargo( CurrentDate.currentDate, true, "Mehmet Aydın", "Photo","Cargo5",CityStorage.getCityById(11));
+        Cargo cargo6 = new Cargo( CurrentDate.currentDate, true, "Semih Bekdaş", "Photo","Cargo6",CityStorage.getCityById(54));
+        Cargo cargo7 = new Cargo( CurrentDate.currentDate, true, "Semih Bekdaş", "Photo","Cargo7",CityStorage.getCityById(22));
+        Cargo cargo8 = new Cargo( CurrentDate.currentDate, true, "Semih Bekdaş", "Photo","Cargo8",CityStorage.getCityById(48));
+        Cargo cargo9 = new Cargo( CurrentDate.currentDate, true, "Semih Bekdaş", "Photo","Cargo9",CityStorage.getCityById(16));
+        Cargo cargo10 = new Cargo( CurrentDate.currentDate, true, "Semih Bekdaş", "Photo","Cargo10",CityStorage.getCityById(64));
+        customer1.addCargo(cargo1);
+        customer2.addCargo(cargo2);
+        customer1.addCargo(cargo3);
+        customer3.addCargo(cargo4);
+        customer1.addCargo(cargo5);
+        customer4.addCargo(cargo6);
+        customer1.addCargo(cargo7);
+        customer1.addCargo(cargo8);
+        customer1.addCargo(cargo9);
+        customer1.addCargo(cargo10);
+
+        CurrentDate.passDay();
+        CargoRouting cr = new CargoRouting();
+        List<Cargo> allCargos=new ArrayList<>(CargoStorage.getAllCargos());
+        cr.routing(allCargos);
+
+
+
         // Create a GraphStream graph
         Graph graph = new SingleGraph("City Routes");
         setupGraphStream(graph, cityGraph);
 
-        // Compute and highlight the shortest route
-        String startCity = "Istanbul";
-        String targetCity = "Mugla";
-        List<String> shortestPath = computeShortestPath(cityGraph, startCity, targetCity);
+        String[] routeHandler=cargo10.getCargoRoute().split(" -> ");
+        List<String> shortestPath = Arrays.asList(routeHandler);
         highlightPath(graph, shortestPath);
 
         // Create a JFrame and embed the graph
